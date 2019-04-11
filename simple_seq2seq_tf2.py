3e4e10da597e25a015919ad578a7cce7fd56826a
Amine Kerkeni <amine.kerkeni@gmail.com>
2019-04-11 12:01:37 +0100

    Draft of python script

---
+++ simple_seq2seq_tf2.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+!pip install tensorflow-gpu==2.0.0-alpha0
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import unicodedata
+import re
+import numpy as np
+import os
+import io
+import time
+import random
+
+
